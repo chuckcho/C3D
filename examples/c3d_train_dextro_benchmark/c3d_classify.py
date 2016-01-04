6edf0b1fe5a4205e0bd5d0809cc2c165fe361422
Chuck Cho <chuck.cho@dextro.co>
2016-01-04 10:07:54 -0500

    a few mods to make pycaffe work

--- examples/c3d_train_dextro_benchmark/c3d_classify.py
+++ examples/c3d_train_dextro_benchmark/c3d_classify.py
@@ -5,67 +5,99 @@ import math
 import cv2
 import scipy.io as sio
 
-def c3d_classify(vid_name, mean_file, net, start_frame=0, num_frames=0):
+def c3d_classify(vid_name, image_mean, net, start_frame=0, num_frames=0):
 
-    num = 25
+    c3d_depth = 16 # num of frames
     num_categories = 131
 
-    if num_frames == 0:
-        imglist = glob.glob(os.path.join(vid_name, '*image_*.jpg'))
-        duration = len(imglist)
-    else:
-        duration = num_frames
-
     # selection
-    step = int(math.floor((duration-1)/(num-1)))
-    dims = (256,340,3,num)
+    dims = (128,171,3,c3d_depth)
     rgb = np.zeros(shape=dims, dtype=np.float64)
     rgb_flip = np.zeros(shape=dims, dtype=np.float64)
 
-    for i in range(num):
-        img_file = os.path.join(vid_name, 'image_{0:05d}.jpg'.format(i*step+1))
+    for i in range(c3d_depth):
+        img_file = os.path.join(vid_name, 'image_{0:05d}.jpg'.format(i+1))
         #print "[Info] img_file={}".format(img_file)
         img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+        #print "[Info] img.shape={}".format(img.shape)
+        img = cv2.resize(img, dims[1::-1])
         rgb[:,:,:,i] = img
         rgb_flip[:,:,:,i] = img[:,::-1,:]
 
-    # crop
-    rgb_1 = rgb[:224, :224, :,:]
-    rgb_2 = rgb[:224, -224:, :,:]
-    rgb_3 = rgb[16:240, 60:284, :,:]
-    rgb_4 = rgb[-224:, :224, :,:]
-    rgb_5 = rgb[-224:, -224:, :,:]
-    rgb_f_1 = rgb_flip[:224, :224, :,:]
-    rgb_f_2 = rgb_flip[:224, -224:, :,:]
-    rgb_f_3 = rgb_flip[16:240, 60:284, :,:]
-    rgb_f_4 = rgb_flip[-224:, :224, :,:]
-    rgb_f_5 = rgb_flip[-224:, -224:, :,:]
-
-    rgb = np.concatenate((rgb_1,rgb_2,rgb_3,rgb_4,rgb_5,rgb_f_1,rgb_f_2,rgb_f_3,rgb_f_4,rgb_f_5), axis=3)
-
     # substract mean
-    d = sio.loadmat(mean_file)
-    image_mean = d['image_mean']
-    #print "[Info] rgb.shape={}".format(rgb.shape)
-    #print "[Info] image_mean.shape={}".format(image_mean.shape)
+    image_mean = np.transpose(image_mean, (2,3,1,0))
+    rgb -= image_mean
+    rgb_flip -= image_mean
 
-    rgb = rgb[:,:,::-1,:] - np.tile(image_mean[...,np.newaxis], (1, 1, 1, rgb.shape[3]))
-    rgb = np.transpose(rgb, (1,0,2,3))
+    # crop
+    rgb_1 = rgb[:112, :112, :,:]
+    rgb_2 = rgb[:112, -112:, :,:]
+    rgb_3 = rgb[8:120, 30:142, :,:]
+    rgb_4 = rgb[-112:, :112, :,:]
+    rgb_5 = rgb[-112:, -112:, :,:]
+    rgb_f_1 = rgb_flip[:112, :112, :,:]
+    rgb_f_2 = rgb_flip[:112, -112:, :,:]
+    rgb_f_3 = rgb_flip[8:120, 30:142, :,:]
+    rgb_f_4 = rgb_flip[-112:, :112, :,:]
+    rgb_f_5 = rgb_flip[-112:, -112:, :,:]
 
-    # test
-    batch_size = 50
-    prediction = np.zeros((num_categories,rgb.shape[3]))
-    num_batches = int(math.ceil(float(rgb.shape[3])/batch_size))
+    rgb = np.concatenate((rgb_1[...,np.newaxis],
+                          rgb_2[...,np.newaxis],
+                          rgb_3[...,np.newaxis],
+                          rgb_4[...,np.newaxis],
+                          rgb_5[...,np.newaxis],
+                          rgb_f_1[...,np.newaxis],
+                          rgb_f_2[...,np.newaxis],
+                          rgb_f_3[...,np.newaxis],
+                          rgb_f_4[...,np.newaxis],
+                          rgb_f_5[...,np.newaxis]), axis=4)
+    #rgb = np.transpose(rgb, (1,0,2,3))
+    #print "net.blobs['data']={}".format(net.blobs['data'])
+    #print "net.blobs['data'].data={}".format(net.blobs['data'].data)
+    #print "net.blobs['data'].data.shape={}".format(net.blobs['data'].data.shape)
+
+    #test_blob = np.zeros(shape=(2,3,16,112,112), dtype=np.float64)
+    #net.blobs['data'].data[...] = test_blob
+    #output = net.forward()
+
+    batch_size = 2
+    prediction = np.zeros((num_categories,rgb.shape[4]))
+    num_batches = int(math.ceil(float(rgb.shape[4])/batch_size))
+    #print "prediction.shape={}, rgb.shape={}, num_batches={}".format(prediction.shape, rgb.shape, num_batches)
 
     for bb in range(num_batches):
-        span = range(batch_size*bb, min(rgb.shape[3],batch_size*(bb+1)))
+        span = range(batch_size*bb, min(rgb.shape[4],batch_size*(bb+1)))
         #print "span={}, len(span)={}".format(span, len(span))
 
-        #print "input={}".format(np.transpose(rgb[:,:,:,span], (3,2,1,0)))
-        net.blobs['data'].data[...] = np.transpose(rgb[:,:,:,span], (3,2,1,0))
+        net.blobs['data'].data[...] = np.transpose(rgb[:,:,:,:,span], (4,2,3,0,1))
         output = net.forward()
-        #print "output={}".format(np.transpose(output['fc8-2']))
+        #print "output['fc8'].shape={}".format(output['fc8'].shape)
 
-        prediction[:, span] = np.transpose(output['fc8-2'])
+        prediction[:, span] = np.transpose(np.squeeze(output['fc8']))
 
     return prediction
+
+'''
+layers {
+  name: "data"
+  type: VIDEO_DATA
+  top: "data"
+  top: "label"
+  image_data_param {
+    source: "dextro_benchmark_val_flow_smaller.txt"
+    use_image: false
+    mean_file: "train01_16_128_171_mean.binaryproto"
+    use_temporal_jitter: false
+    #batch_size: 30
+    #batch_size: 2
+    batch_size: 2
+    crop_size: 112
+    mirror: false
+    show_data: 0
+    new_height: 128
+    new_width: 171
+    new_length: 16
+    shuffle: true
+  }
+}
+'''
--- examples/c3d_train_dextro_benchmark/run_detections_dextro_benchmark.py
+++ examples/c3d_train_dextro_benchmark/run_detections_dextro_benchmark.py
@@ -8,12 +8,9 @@ import random
 import json
 
 import sys
+#sys.path.append("/home/chuck/projects/caffe/cmake_build/install/python")
 sys.path.append("/home/chuck/projects/C3D/python")
-try:
-    import caffe
-except ImportError:
-    print 'ImportError'
-    import caffe
+import caffe
 
 from c3d_classify import c3d_classify
 
@@ -45,15 +42,16 @@ def main():
 
     print "allmodules=".format(allmodules)
 
+    # model
+    model_def_file = './c3d_dextro_benchmark_test.prototxt'
+    model_file = 'c3d_dextro_benchmark_iter_5000'
+    net = caffe.Net(model_def_file, model_file)
+
     # caffe init
     gpu_id = 0
-    caffe.set_device(gpu_id)
-    caffe.set_mode_gpu()
-
-    # model
-    model_def_file = 'c3d_ucf101_finetuning_test.prototxt'
-    model_file = 'c3d_dextro_benchmark_iter_5000'
-    net = caffe.Net(model_def_file, model_file, caffe.TEST)
+    net.set_device(gpu_id)
+    net.set_mode_gpu()
+    net.set_phase_test()
 
     # read test video list
     test_video_list = 'dextro_benchmark_val_flow_smaller.txt'
@@ -61,30 +59,38 @@ def main():
     reader = csv.reader(open(test_video_list), delimiter=" ")
 
     # top_N
-    top_N = 5
+    top_N = 10
     #top_N = 131 # for debugging
 
     content_type_mapping, content_type_id_mapping = get_content_type()
 
     for count, video_and_category in enumerate(reader):
-        (video_name, start_frame, num_frames, category) = video_and_category
+        (video_file, start_frame, category) = video_and_category
+        video_name = os.path.splitext(video_file)[0]
         start_frame = int(start_frame)
-        num_frames = int(num_frames)
         category = int(category)
         if not os.path.isdir(video_name):
             print "[Error] video_name path={} does not exist. Skipping...".format(video_name)
             continue
         video_id = video_name.split('/')[-1]
         print "-"*79
-        print "video_name={} ({}-th), start_frame={}, num_frames={}, category={}".format(video_name, count+1, start_frame, num_frames, category)
+        print "video_name={} ({}-th), start_frame={}, category={}".format(video_name, count+1, start_frame, category)
 
-        result = '{0}_frame_{1:05d}_numframe_{2:05d}_category_{3:04d}_rgb.txt'.format(video_id, start_frame, num_frames, category)
+        result = '{0}_frame_{1:05d}_category_{2:04d}_c3d.txt'.format(video_id, start_frame, category)
         if os.path.isfile(result) and not force_save:
             print "[Info] intermediate output file={} for RGB has been already saved. Skipping...".format(result)
             avg_pred = np.loadtxt(result)
         else:
-            mean_file = 'mean.mat'
-            prediction = VideoSpatialPrediction(video_name, mean_file, net, start_frame, num_frames)
+            mean_file = './train01_16_128_171_mean.binaryproto'
+            blob = caffe.proto.caffe_pb2.BlobProto()
+            data = open(mean_file,'rb').read()
+            blob.ParseFromString(data)
+            blob.num = 16
+            #print "blob={}".format(blob)
+            #print "len(blob.data)={}".format(len(blob.data))
+            #print "blob.num={}, blob.channels={}, blob.height={}, blob.width={}".format(blob.num, blob.channels, blob.height, blob.width)
+            image_mean = np.array(caffe.io.blobproto_to_array(blob))
+            prediction = c3d_classify(video_name, image_mean, net, start_frame)
             avg_pred_fc8 = np.mean(prediction, axis=1)
             avg_pred = softmax(avg_pred_fc8)
             np.savetxt(result, avg_pred, delimiter=",")
@@ -101,3 +107,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''
+layers {
+  name: "data"
+  type: VIDEO_DATA
+  top: "data"
+  top: "label"
+  image_data_param {
+    source: "dextro_benchmark_val_flow_smaller.txt"
+    use_image: false
+    mean_file: "train01_16_128_171_mean.binaryproto"
+    use_temporal_jitter: false
+    #batch_size: 30
+    #batch_size: 2
+    batch_size: 2
+    crop_size: 112
+    mirror: false
+    show_data: 0
+    new_height: 128
+    new_width: 171
+    new_length: 16
+    shuffle: true
+  }
+}
+'''
